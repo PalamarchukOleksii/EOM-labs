59ffb62a668f96ef4e4c9db4e593c20d45cf2bb2
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-03-12 01:03:04 +0200

    add function to sort population based on function value

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 P = 10
 
@@ -10,6 +11,14 @@ lb_f4 = {"x": -10, "y": -10}
 ub_f4 = {"x": 10, "y": 10}
 
 param_list = ["x", "y"]
+
+
+def f4(x, y):
+    return (
+        (math.sin(3 * math.pi * x)) ** 2
+        + ((x - 1) ** 2 * (1 + (math.sin(3 * math.pi * y)) ** 2))
+        + ((y - 1) ** 2 * (1 + (math.sin(2 * math.pi * y)) ** 2))
+    )
 
 
 def generate_population(lower_bound, upper_bound, population_size, parameters_list):
@@ -29,7 +38,23 @@ def generate_population(lower_bound, upper_bound, population_size, parameters_li
     return population
 
 
+def sort_population(function, population):
+    population_values = []
+    for point in population:
+        population_values.append((point, function(point[0], point[1])))
+
+    population_values.sort(key=lambda x: x[1])
+
+    sorted_population = []
+    for value in population_values:
+        sorted_population.append(value[0])
+
+    return sorted_population
+
+
 population = generate_population(lb_f4, ub_f4, P, param_list)
 
-for point in population:
-    print(point)
+sorted_population = sort_population(f4, population)
+
+for el in sorted_population:
+    print(el)
